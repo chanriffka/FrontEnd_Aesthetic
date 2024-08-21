9b1fdd7074a0d4fd8e2bf07b892149c471ba0423
sintaa27 <yasintaprim@gmail.com>
2024-08-21 17:59:52 +0700

    fix category

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.css'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'aesthetic': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
